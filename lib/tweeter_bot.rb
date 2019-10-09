b149ce9375f75912123aaafc59c4d32ea2047f74
ludo <ludovict40@gmail.com>
2019-10-09 13:28:58 +0200

    ajout spec et modif fichier rb

--- lib/tweeter_bot.rb
+++ lib/tweeter_bot.rb
@@ -1,14 +1,21 @@
 require 'pry'
 require 'dotenv'
+require 'twitter'
 
-def multiply_by_6(var) #définition d'une méthode multipliant par 6 en 2 étapes
-    var = var * 2
-    #binding.pry # On lance PRY au milieu de la méthode
-    var = var * 3
-    return var
-end
+#binding.pry # On lance PRY au milieu de la méthode
 
 Dotenv.load('.env') # appelle le fichier .env
 
-puts ENV['TWITTER_API_SECRET'] # récupère les éléments du fichier .env
-puts ENV['BEST_WEBSITE_EVER']
+#puts ENV['TWITTER_API_SECRET'] # récupère les éléments du fichier .env
+#puts ENV['BEST_WEBSITE_EVER']
+def login_twitter
+    client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+        config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+        config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+        config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+    end
+    return client
+end
+
+#client.update('Mon premier tweet en ruby')
